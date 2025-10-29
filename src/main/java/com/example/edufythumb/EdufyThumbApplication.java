8be36177db011721be24665170d981161e9ececd
Alex <alexander.westerberg.svedberg@gmail.com>
2025-10-29 13:39:31 +0100

    first commit

---
+++ src/main/java/com/example/edufythumb/EdufyThumbApplication.java
@@ -0,0 +1,13 @@
+package com.example.edufythumb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyThumbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdufyThumbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/edufythumb/EdufyThumbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edufythumb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufyThumbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
