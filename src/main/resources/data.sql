297ea83f65df824c4c2a03c4228056fb5f87b122
Sommar-skog <amandaaronsson@hotmail.com>
2025-11-07 15:54:19 +0100

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,10 +11,18 @@ VALUES
 -- Add userId that voted thumbsUp
 INSERT INTO user_votes_thumb_up (thumb_id, user_id_thumbs_up)
 VALUES
-    (1, 101),
-    (1, 102),
-    (1, 103),
-    (2, 104),
-    (3, 105),
-    (3, 106),
-    (4, 107);
+-- Blue
+(1, 101),
+(1, 102),
+(1, 103),
+-- Thunderstruck
+(2, 104),
+(2, 105),
+-- Funny Cats Compilation
+(3, 106),
+(3, 107),
+(3, 108),
+(3, 109),
+-- Tech Weekly: AI Edition
+(4, 110),
+(4, 111);
