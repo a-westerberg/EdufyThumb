35762b6cc7fca12757eaa1d39fac8a6f79e0d163
Sommar-skog <amandaaronsson@hotmail.com>
2025-11-10 08:28:07 +0100

    updated data.sql with thumbs-down-votes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,13 +16,31 @@ VALUES
 (1, 2),
 (1, 3),
 -- Thunderstruck
+(2, 1),
 (2, 4),
 (2, 5),
 -- Funny Cats Compilation
 (3, 6),
 (3, 7),
 (3, 8),
-(3, 9),
 -- Tech Weekly: AI Edition
+(4, 9),
 (4, 10),
 (4, 1);
+
+--ED-105-AA
+-- Add userId that voted thumbsDown
+INSERT INTO user_votes_thumb_down (thumb_id, user_id_thumbs_down)
+VALUES
+-- Blue
+(1, 4),
+(1, 5),
+-- Thunderstruck
+(2, 6),
+-- Funny Cats Compilation
+(3, 9),
+(3, 10),
+-- Tech Weekly: AI Edition
+(4, 2),
+(4, 3),
+(4, 5);
