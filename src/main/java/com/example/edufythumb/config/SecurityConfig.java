6104e6dd441c0464d3f2b31d7c9a7e6afe782969
Alex <alexander.westerberg.svedberg@gmail.com>
2025-11-13 14:34:22 +0100

    commit innan merge

--- src/main/java/com/example/edufythumb/config/SecurityConfig.java
+++ src/main/java/com/example/edufythumb/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
+                        .jwt(Customizer.withDefaults())     //om admin endpoints tillkommer framöver behövs detta justeras
                 );
 
         return http.build();
